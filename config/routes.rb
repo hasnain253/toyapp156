29a04ccdd5e53d0c4757038e8073003baf60c961
Your Name <you@example.com>
2022-11-25 18:25:36 +0500

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+"Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'application#hello'
+end"
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :microposts
+  resources :users
+  root 'users#index'
 end
